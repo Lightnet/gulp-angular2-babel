f23621e8a6ffcd1310dc685f8638773161501aec
Lightnet <darkneter@gmail.com>
2017-01-04 10:24:32 -0800

    cleaup and testing.

--- src/services.js
+++ src/services.js
@@ -1,15 +1,15 @@
 //simple class greeter
 
 export class Greeter {
-  say(greeting, name) {
-    const capitalized = this.capitalize(greeting);
-    return `${capitalized}, ${name}!`;
-  }
-
-  capitalize(str) {
-    if (!str) {
-      return str;
+    say(greeting, name) {
+        const capitalized = this.capitalize(greeting);
+        return `${capitalized}, ${name}!`;
     }
-    return str.replace(/^(.)/, c => c.toUpperCase());
-  }
+
+    capitalize(str) {
+        if (!str) {
+            return str;
+        }
+        return str.replace(/^(.)/, c => c.toUpperCase());
+    }
 }
